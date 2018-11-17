fdc2dbbb1e32033ea22ed2c25a22c81cda7d8351
beeinpeace1 <beeinpeace1@gmail.com>
2018-11-17 16:46:35 +0530

    landing bg fix

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -2,25 +2,32 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import "./Landing.css";
 import Background from "./../../assets/images/landing/background.jpg";
+import Lines from "./../../assets/images/landing/lines.jpg";
 import Logo from "./../../assets/images/landing/logo.png";
 
 class Landing extends React.Component {
   componentDidMount(){
     setTimeout(() => {
-      this.props.history.push("/products");
+      // this.props.history.push("/products");
     }, 4000);
   }
   
   render() {
     return (
-      <div
+      <div style={{
+        backgroundImage: `url(${Lines})`,
+      }}>
+              <div
         id="landing-page"
         style={{
           backgroundImage: `url(${Background})`,
+          backgroundRepeat: "no-repeat"
         }}
       >
         <img src={Logo} alt="img" id="landing-page-logo" />
       </div>
+      </div>
+
     );
   }
 }
